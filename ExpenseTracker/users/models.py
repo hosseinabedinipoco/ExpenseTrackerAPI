73b502ef639c037a44168da84959b7b1a5c5672a
Hossein <hosseinabedinipoco@gmail.com>
2024-09-17 15:56:50 +0330

    create user model

--- ExpenseTracker/users/models.py
+++ ExpenseTracker/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(max_length=50, unique=True)
+    password = models.CharField(max_length=300)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
