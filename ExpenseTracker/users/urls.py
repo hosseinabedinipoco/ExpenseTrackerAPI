37d42e94bc5348704db00be80c9ab727f1bdec0f
Hossein <hosseinabedinipoco@gmail.com>
2024-09-17 17:00:36 +0330

    add send otp view

--- ExpenseTracker/users/urls.py
+++ ExpenseTracker/users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('login', views.login.as_view(), name='login'),
     path('signup', views.signup.as_view(), name='signup'),
+    path('send_otp', views.send_otp.as_view(), name='send')
 ]
--- ExpenseTracker/users/views.py
+++ ExpenseTracker/users/views.py
@@ -6,7 +6,12 @@ from .models import User
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
+import redis
+import smtplib, ssl, random
+
 # Create your views here.
+
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 class login(APIView):
     permission_classes = [AllowAny]
@@ -24,4 +29,24 @@ class login(APIView):
 
 
 class signup(APIView):
-    pass
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        pass
+
+class send_otp(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        port = 465  # For SSL
+        smtp_server = "smtp.gmail.com"
+        sender_email = "hosseinabedinipoco@gmail.com"  # Enter your address
+        receiver_email = request.data.get('email')  # Enter receiver address
+        password = "rauxzpjalhevxjvz"
+        num = random.randint(1000, 9999)
+        message = str(num)
+        r.setex(receiver_email, message, 120)
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message)
+        return Response({'message':'seccusful'}, status=status.HTTP_200_OK)    
