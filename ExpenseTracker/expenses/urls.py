4192bba910fc2983efa89906ee07527081aec7d4
Hossein <hosseinabedinipoco@gmail.com>
2024-09-19 00:16:00 +0330

    add get all expenses view

--- ExpenseTracker/expenses/urls.py
+++ ExpenseTracker/expenses/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('add', views.AddExpense.as_view()),
     path('get_all', views.GetAllExpense.as_view()),
     path('delete/<int:id>', views.DeleteExpense.as_view()),
-    path('update/<int:id>', views.UpdateExpense.as_view())
+    path('update/<int:id>', views.UpdateExpense.as_view()),
+    path('get/<int:id>', views.GetExpense.as_view())
 ]
